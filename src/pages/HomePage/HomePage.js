ecc7659571c56bc95f1ddbd8eab06429f63daceb
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-04-05 13:03:26 +0100

    add images for the home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./HomePage.scss";
+import homePageTop from "../../assets/images/new-home-top.png";
+import homePageBottom from "../../assets/images/new-home-bottom.png";
 import LatestFeature from "../../components/LatestFeature/LatestFeature";
 
 export default function HomePage() {
   return (
-    <main>
-      <LatestFeature />
+    <main className="main">
+      <img
+        className="main__img "
+        src={homePageTop}
+        alt="image of the app home screen"
+      />
+      <LatestFeature className="main__feature" />
+      <img
+        className="main__img "
+        src={homePageBottom}
+        alt="image of the app home screen"
+      />
     </main>
   );
 }
