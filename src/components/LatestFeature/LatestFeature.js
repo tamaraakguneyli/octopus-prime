b8c71c03e8e79b70802830dc7f715b8d766ce336
tamaraakguneyli <tamara.akguneyli@hotmail.co.uk>
2024-04-05 09:48:13 +0100

    add structure for home page

---
+++ src/components/LatestFeature/LatestFeature.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./LatestFeature.scss";
+
+export default function LatestFeature() {
+  return;
+  <></>;
+}
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./HomePage.scss";
+
+export default function HomePage() {
+  return;
+  <main></main>;
+}
