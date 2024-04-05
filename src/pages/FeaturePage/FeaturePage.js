bea9c4a5473b1a79ba4bb2f63490bbaf5afbf767
Sarah Naunton <sarah@naunton.org>
2024-04-05 13:42:10 +0100

    add phone header to page

--- src/pages/FeaturePage/FeaturePage.js
+++ src/pages/FeaturePage/FeaturePage.js
@@ -1,5 +1,6 @@
 import Benefits from "../../components/Benefits/Benefits";
 import Reminder from "../../components/Reminder/Reminder";
+import header from "../../assets/images/home-header.png";
 import { Link } from "react-router-dom";
 import "./FeaturePage.scss";
 
@@ -7,14 +8,17 @@ export default function FeaturePage() {
   return (
     <>
       <header className="feature-page">
+        <img src={header} alt="Iphone top of screen" className="feature__image" />
         <h1 className="feature-page__heading">Introducing</h1>
         <h2 className="feature-page__subheading">Smart Notifications</h2>
       </header>
       <main className="feature-page__contents">
-        <Reminder/>
-        <Benefits/>
+        <Reminder />
+        <Benefits />
         <div className="feature-page__buttons">
-          <Link to="/loading"><button className="feature-page__enable">Enable</button></Link>
+          <Link to="/loading">
+            <button className="feature-page__enable">Enable</button>
+          </Link>
           <button className="feature-page__not">Not Now</button>
         </div>
       </main>
