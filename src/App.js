dd638b974d6ce246e788a24c2a0ebfd5d116a4d5
Leon Williams <leonbwilliams6@gmail.com>
2024-04-05 15:09:01 +0100

    Add energy graph component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import FeaturePage from "./pages/FeaturePage/FeaturePage";
 import "./App.scss";
 import Loader from "./components/Loader/Loader";
+import GraphPage from "./pages/GraphPage/GraphPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/feature" element={<FeaturePage />} />
         <Route path="/loading" element={<Loader />} />
+        <Route path="/energy" element={<GraphPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/GraphPage/GraphPage.js
+++ src/pages/GraphPage/GraphPage.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Graph from "../../components/Graph/Graph";
 
 export default function GraphPage() {
-  return <></>;
+  return <>
+  <Graph/>
+  </>;
 }
